e94ed4c576ddaaaadded2fd74f894b6fd18271ce
“uncoding” <foorwork@163.com>
2015-06-22 23:08:07 +0800

    test

---
+++ data/menuselectcache.php
@@ -0,0 +1,92 @@
+<?php
+$tableInfo = array (
+  1 => 
+  array (
+    'id' => '1',
+    'pid' => '0',
+    'name' => '网站首页',
+    'link' => '/',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  2 => 
+  array (
+    'id' => '2',
+    'pid' => '0',
+    'name' => '公司简介',
+    'link' => '/index.php?act=aboutus.aboutus&cid=2',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  3 => 
+  array (
+    'id' => '3',
+    'pid' => '0',
+    'name' => '新闻动态',
+    'link' => '/index.php?act=news.news&cid=3',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  5 => 
+  array (
+    'id' => '5',
+    'pid' => '0',
+    'name' => '服务项目',
+    'link' => '/index.php?act=server.server&cid=4',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  8 => 
+  array (
+    'id' => '8',
+    'pid' => '0',
+    'name' => '案例展示',
+    'link' => '/index.php?act=casese.casese&cid=5',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  10 => 
+  array (
+    'id' => '10',
+    'pid' => '0',
+    'name' => '人才招聘',
+    'link' => '/index.php?act=recruit.recruit&cid=6',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  9 => 
+  array (
+    'id' => '9',
+    'pid' => '0',
+    'name' => '联系我们',
+    'link' => '/index.php?act=contact.contact&cid=6',
+    'isselect' => 1,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+  11 => 
+  array (
+    'id' => '11',
+    'pid' => '0',
+    'name' => '产品展示',
+    'link' => '/index.php?act=photo.photo&cid=9',
+    'isselect' => 0,
+    'sub' => 0,
+    'lever' => 1,
+    'prestr' => '-',
+  ),
+);
+?>
